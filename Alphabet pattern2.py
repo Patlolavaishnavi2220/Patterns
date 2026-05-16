76f5e6fdccae0a0060660223abc1b2e2ce294caa
Patalula vaishnavi <patlolavaishnavi1@gmail.com>
2026-05-16 20:31:56 +0530

    Add alphabet pattern printing functionality
    Implement a pattern printing program using letters.

---
+++ Alphabet pattern2.py
@@ -0,0 +1,16 @@
+n=int(input())
+c=65
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print(chr(c),end=" ")
+        c=c+1
+    print()
+'''output:
+4
+      A 
+    B C 
+  D E F 
+G H I J 
+'''
