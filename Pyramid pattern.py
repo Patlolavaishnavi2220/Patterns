07738235c3af1de7ae7070f84e86b8e59e9136c1
Patalula vaishnavi <patlolavaishnavi1@gmail.com>
2026-05-16 20:53:36 +0530

    Create Pyramid pattern.py for printing stars
    Add a pyramid pattern printing script in Python.

---
+++ Pyramid pattern.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(1, n + 1):
+
+    # spaces
+    for j in range(n - i):
+        print(" ", end="")
+
+    # stars
+    for j in range(i):
+        print("* ", end="")
+
+    print()
+'''output:
+4
+   * 
+  * * 
+ * * * 
+* * * * 
+'''
